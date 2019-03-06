78f5b30a51024223df3e375b6a7d41fb45b1b3cb
PikkamanV <t-miura@mfac.jp>
2019-03-06 12:35:36 +0900

    feat(backend/python): gariを参考にJSON serializerを入れて動くようにした

--- messages/step-2/backend/python/server.py
+++ messages/step-2/backend/python/server.py
@@ -1,61 +1,74 @@
 import json
 from bottle import route, run, request, response, static_file, hook
 from pymemcache.client import Client
-import logging
-import json
-import ast
 
 DB_HOST = 'localhost'
 DB_PORT = 5652
 APP_HOST = 'localhost'
 APP_PORT = 5650
 
-db = Client((DB_HOST, DB_PORT), default_noreply=False)
+class Dao:
+    def __init__(self, host, port):
+        self.db = Client(
+            (host, port),
+            default_noreply=False,
+            serializer=self.__json_serializer,
+            deserializer=self.__json_deserializer
+        )
 
-def get_count():
-    count = db.get('count').decode('utf8')
-    # if type(count) is 'str':
-    return int(count)
-    # else:
-        # return 0
+    def __json_serializer(self, key, value):
+        if type(value) == str:
+            return value, 1
+        return json.dumps(value), 2
 
-def get_messages(count):
-    ids = range(1, count + 1)
-    neko = db.get_multi([f'messages:{id}' for id in ids])
-    inu = [{'id': id, **(ast.literal_eval(neko[f'messages:{id}'].decode('utf-8')))} for id in ids]
-    return inu
+    def __json_deserializer(self, key, value, flags):
+        if flags == 1:
+            return value.decode('utf-8')
+        if flags == 2:
+            return json.loads(value.decode('utf-8'))
+        raise Exception('Unknown serialization format')
+
+    def get_count(self):
+        count = self.db.get('count')
+        return int(count)
+
+    def get_messages(self, count):
+        ids = range(1, count+1)
+        result = self.db.get_multi([f'messages:{id}' for id in ids])
+        return [{'id': id, **result[f'messages:{id}']} for id in ids]
+
+    def incr_count(self):
+        count = self.db.get('count')
+        if count:
+            return self.db.incr('count', 1)
+        else:
+            self.db.set('count', 1)
+            return 1
+
+    def set_message(self, count, uniqys_request):
+        self.db.set('messages:'+str(count), uniqys_request)
+
+dao = Dao(DB_HOST, DB_PORT)
 
 @route('/api/message')
 def get_message():
-    count = get_count()
-    messages = get_messages(count)
+    count = dao.get_count()
+    messages = dao.get_messages(count)
     return {'messages': messages}
-
-def incr_count():
-    count = db.get('count')
-    if count:
-        return db.incr('count', 1)
-    else:
-        db.set('count', 1)
-        return 1
 
 
 @route('/api/message', method='POST')
 def post_message():
-    sender = request.get_header('uniqys-sender')
-    timestamp = request.get_header('uniqys-timestamp')
-    blockhash = request.get_header('uniqys-blockhash')
-
+    count = dao.incr_count()
     body = request.json
-    message = body['message']
 
-    count = incr_count()
+    uniqys_request = {
+            'sender': request.get_header('uniqys-sender'),
+            'timestamp': request.get_header('uniqys-timestamp'),
+            'blockhash': request.get_header('uniqys-blockhash'),
+            'contents': body['message']
+    }
 
-    db.set('messages:'+str(count), {
-        "sender": sender,
-        "timestamp": timestamp,
-        "blockhash": blockhash,
-        "contents": message.encode('utf-8')
-    })
+    dao.set_message(count, uniqys_request)
 
 run(host=APP_HOST, port=APP_PORT, debug=True, reloader=True)
