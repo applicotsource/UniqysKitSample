c25b046c6e60fafbfd006322861a77a683f0e150
nanotumblr <peropoyi@gmail.com>
2019-03-06 11:15:18 +0900

    style for mock

--- sushi/step-1/README.md
+++ sushi/step-1/README.md
@@ -3,3 +3,58 @@ vue create frontend
 cd frontend
 npm run serve
 ```
+
+App.vueのコンポーネントを消す
+
+自分のアドレスを作ってみる
+```
+myAddress: '0xhogehoge'
+```
+
+おすしのデータを作ってみる
+```
+data() {
+  return {
+    sushiList: [
+      {
+        status: 'sell' | 'normal',
+        price: number,
+        owner: string,
+        dna: string
+      },
+      ...
+    ]
+  }
+}
+```
+
+おすしの枠を表示してみる
+
+```
+<div v-for="sushi in sushiList" :key="sushi.id">
+      <p>{{ sushi.status }}</p>
+      <p>{{ sushi.price }}</p>
+      <p>{{ sushi.owner }}</p>
+      <p>{{ sushi.dna }}</p>
+    </div>
+    ```
+
+styleを当ててみる
+```
+    <div class="sushi-wrapper">
+      <div class="sushi-box" v-for="sushi in sushiList" :key="sushi.id">
+        <p>{{ sushi.status }}</p>
+        <p>{{ sushi.price }}</p>
+        <p>{{ sushi.owner }}</p>
+        <p>{{ sushi.dna }}</p>
+      </div>
+    </div>
+
+.sushi-wrapper {
+  display: flex;
+}
+.sushi-box {
+  border: 1px solid black;
+}
+```
+
--- sushi/step-1/frontend/src/App.vue
+++ sushi/step-1/frontend/src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="sushi-wrapper">
+      <div class="sushi-box" v-for="sushi in sushiList" :key="sushi.id">
+        <p>{{ sushi.status }}</p>
+        <p>{{ sushi.price }}</p>
+        <p>{{ sushi.owner }}</p>
+        <p>{{ sushi.dna }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      myAddress: '0xhogehoge',
+      sushiList: [
+        { // 自分の販売中じゃないおすし
+          id: 1,
+          status: 'normal',
+          price: 0,
+          owner: '0xhogehoge',
+          dna: 'irjiorgoiwegjioergj'
+        },
+        { // 自分の販売中のおすし
+          id: 2,
+          status: 'sell',
+          price: 0,
+          owner: '0xhogehoge',
+          dna: '0rtihij6i45h4jgioijerf'
+        },
+        { // 他の人の販売中じゃないおすし
+          id: 3,
+          status: 'normal',
+          price: 0,
+          owner: '0xhugahuga',
+          dna: 'x3igwegjsij5gjj35p4hi45h'
+        },
+        { // 他の人の販売中のおすし
+          id: 4,
+          status: 'sell',
+          price: 0,
+          owner: '0xhugahuga',
+          dna: 'irjiorgoiwegjioergj'
+        },
+      ]
+    }
   }
 }
 </script>
@@ -24,5 +61,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.sushi-wrapper {
+  display: flex;
+}
+.sushi-box {
+  border: 1px solid black;
 }
 </style>
